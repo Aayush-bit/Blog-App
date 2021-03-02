26527d37f420ce2dca6f7a008d7ec6eeb4c6605d
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-03-02 20:04:58 +0530

    made basic change in /user endpoint in the server

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Router = express.Router()
 const auth = require('./auth')
 
-const usersData = require('./data/usersData')
+// const usersData = require('./data/usersData')
 
 Router.get('/', auth, (req, res) => {
     res.send(req.user)
