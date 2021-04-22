381abe8326d5246fe74ca20f3a1b1d733f619c22
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-04-22 19:39:48 +0530

    added loader to the users page

--- client/src/pages/Users/Users.js
+++ client/src/pages/Users/Users.js
@@ -2,13 +2,16 @@
 
 import React, {useState, useEffect, useContext} from 'react'
 import { useHistory } from 'react-router-dom'
-// import UserCard from '../../../components/usersPage/UserCard'
+import Loader from "react-loader-spinner";
 import axios from 'axios'
+
+import UserCard from '../../components/usersPage/UserCard'
 import { LoggedInContext } from '../../App'
 
 function Users() {
     const [users, setUsers] = useState([]) // for storing the users data
     // const [err, setErr] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
     const [loggedIn, setLoggedIn] = useContext(LoggedInContext)
     const history = useHistory()
 
@@ -16,7 +19,10 @@ function Users() {
 
     useEffect(() => {
         axios.get(url)
-        .then(res => setUsers(res.data))
+        .then(res => {
+            setUsers(res.data);
+            setIsLoading(false);
+        })
         .catch(resErr => {
             console.error(resErr);
             if(resErr.response.status === 401) {
@@ -31,6 +37,16 @@ function Users() {
         <div className="container">
             <h1 className='display-4'>Users</h1>
             {
+                // apply loading spinner
+                isLoading ? 
+                <>
+                    <Loader
+                        type="Rings"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                    />
+                </> :
                 users.map((user, index) => (
                     <React.Fragment key={index}>
                         <hr/>
