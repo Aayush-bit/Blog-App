0258a2720c29c1d9ce0f68cdef43107b194dcd87
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-02 10:55:48 +0530

    removed 401 unautherized access from catch error statement in the response of api call to '/api/users' and improved loader styling

--- client/src/pages/Users/Users.js
+++ client/src/pages/Users/Users.js
@@ -1,21 +1,16 @@
 // !! reconsider api values returned as response from server
 
-import React, {useState, useEffect, useContext} from 'react'
-import { useHistory } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
 import Loader from "react-loader-spinner";
 import axios from 'axios'
 
-import UserCard from '../../components/usersPage/UserCard'
-import { LoggedInContext } from '../../App'
+// import UserCard from '../../components/usersPage/UserCard'
 
 function Users() {
     const [users, setUsers] = useState([]) // for storing the users data
-    // const [err, setErr] = useState([])
+    // ? const [err, setErr] = useState([])
     const [isLoading, setIsLoading] = useState(true);
-    const [loggedIn, setLoggedIn] = useContext(LoggedInContext)
-    const history = useHistory()
-
-    const url = '/api/users'
+    const url = '/api/users';
 
     useEffect(() => {
         axios.get(url)
@@ -23,29 +18,23 @@ function Users() {
             setUsers(res.data);
             setIsLoading(false);
         })
-        .catch(resErr => {
-            console.error(resErr);
-            if(resErr.response.status === 401) {
-                // set login status to false and redirect to login page
-                setLoggedIn(false);
-                history.push('/login');            
-            }
-        })
+        .catch(resErr => console.error(resErr))
     }, [])
 
     return (
         <div className="container">
             <h1 className='display-4'>Users</h1>
             {
-                // apply loading spinner
                 isLoading ? 
                 <>
-                    <Loader
-                        type="Rings"
-                        color="#00BFFF"
-                        height={100}
-                        width={100}
-                    />
+                    <div className="loader">
+                        <Loader
+                            type="TailSpin"
+                            color="#343a40"
+                            height={70}
+                            width={70}
+                        />
+                    </div>
                 </> :
                 users.map((user, index) => (
                     <React.Fragment key={index}>
