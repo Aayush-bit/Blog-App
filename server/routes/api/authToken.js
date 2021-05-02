5977221d52facafe7ded9335657c278c6ab96c5d
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-02 10:19:26 +0530

    collected access token from cookies

--- server/routes/api/authToken.js
+++ server/routes/api/authToken.js
@@ -2,10 +2,7 @@ const jwt = require('jsonwebtoken');
 
 // middleware to verify the access token from the request
 const authToken = (req, res, next) => {
-
-    const authHeader = req.headers['authorization'];
-    const accessToken = authHeader && authHeader.split(' ')[1];
-
+    const accessToken = req.cookies.accessToken;
     // if no access token was sent
     if (accessToken == null) return res.sendStatus(401);
 
@@ -13,7 +10,7 @@ const authToken = (req, res, next) => {
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) return res.status(403).send('token didnt verify')
         next();
-    })    
+    })
 }
 
 module.exports = authToken
