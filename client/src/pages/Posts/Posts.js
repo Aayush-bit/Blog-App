7925a6753d214f875c57204ee09bda7b6c2f591d
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-02 23:41:14 +0530

    added page loader and requested some data from the api

--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import PageLoader from "../../components/PageLoader"
 
 function Posts() {
+    const [posts, setPosts] = useState([]) // for storing posts data
+    // ? const [err, setErr] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const url = '/api/posts';
+
+    useEffect(() => {
+        axios.get(url)
+        .then(res => {
+            setPosts(res.data);
+            setIsLoading(false);
+        })
+        .catch(resErr => console.error(resErr))
+    }, [])
+
     return (
         <div className="container">
             <h1 className="display-4">Posts</h1>
-            
+            {
+                isLoading ? 
+                <PageLoader /> :
+                // posts.map((postData, index) => (
+                //     (postData.posts !== []) ?
+                //     <React.Fragment key={index}>
+                //         <hr/>
+                //         {JSON.stringify(postData.posts)}
+                //         <br/>
+                //     </React.Fragment> : null
+                // ))
+                <>
+                    <p>data loaded</p>
+                    {console.log(posts)}
+                </>
+            }
         </div>
     )
 }
--- client/src/pages/private/profilePage/Profile.js
+++ client/src/pages/private/profilePage/Profile.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import PageLoader from "../../../components/PageLoader";
+import { useCookies } from 'react-cookie';
 
-function Profile() {
+const Profile = () => {
+    const [profileData, setProfileData] = useState([]);     // for storing profileUserData data
+    // ? const [err, setErr] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [cookies, setCookie, deleteCookie] = useCookies(['userId']);
+    const url = `/api/users/${cookies.userId}`;
+
+    // todo make request to get the posts posted by the user
+    useEffect(() => {
+        axios.get(url)
+        .then(res => {
+            setProfileData(res.data);
+            setIsLoading(false);
+        })
+        .catch(resErr => console.error(resErr))
+    }, [])
+
     return (
         <div className="container">
             <h1 className="display-4">Profile</h1>
+            {
+                isLoading ? 
+                <PageLoader /> :
+                <>
+                    {console.log(profileData)}
+                    <p>username: {profileData.name}</p>
+                    <p>email: {profileData.email}</p>
+                    <p>followers: {profileData.followers.length}</p>
+                    <p>following: {profileData.following.length}</p>
+                    <p>bookmarks: {profileData.bookmarks.length}</p>
+                </>
+            }
         </div>
     )
 }
