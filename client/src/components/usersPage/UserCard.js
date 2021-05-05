184a685537c61e8b29f0286755f5d2025d6952d5
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-05 13:46:58 +0530

    add more data to show inside the users card, made follow/unfollow feature only for UI not for the database

--- client/src/components/usersPage/UserCard.js
+++ client/src/components/usersPage/UserCard.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Card, Button } from 'react-bootstrap'
 
-function UserCard({ name, email }) {
+function UserCard({ name, profileImg, email, bio }) {
+    const [isFollowing, setIsFollowing] = useState(false);
+    const [followBtnVariant, setFollowBtnVariant] = useState("success");
+    const [followBtnMessage, setFollowBtnMessage] = useState("Follow");
+
+    // some brief of users bio
+    const bioBrief = (bio.length >= 35) ? bio.substring(0,35) + "..." : bio;
+
+    const handleFollowStatusChange = () => {
+        if(isFollowing === false) {
+            setIsFollowing(true);
+            setFollowBtnVariant("danger");
+            setFollowBtnMessage("Unfollow");
+        }
+        else if(isFollowing === true) {
+            setIsFollowing(false);
+            setFollowBtnVariant("success");
+            setFollowBtnMessage("Follow");
+        }
+    }
+
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" />
+            <Card className="mb-3" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={profileImg} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
-                    <Card.Text>{email}</Card.Text>
-                    <Button variant="success">Follow</Button>
+                    <Card.Text>Email: {email}</Card.Text>
+                    {bio ? <Card.Text className="text-muted">{bioBrief}</Card.Text> : null}
+                    <Button variant={followBtnVariant} onClick={handleFollowStatusChange}>{followBtnMessage}</Button>
+                    <Button variant="info" className="ml-3">Profile</Button>
                 </Card.Body>
             </Card>
         </div>
