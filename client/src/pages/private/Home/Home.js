6a3a6154d6ba5f21c4b2b94cf052ecad97fdd84f
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-09 00:34:49 +0530

    minor changes

--- client/src/pages/private/Home/Home.js
+++ client/src/pages/private/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Home() {
     return (
         <div className="container">
             <h1 className="display-4">Private Home page</h1>
+            <Link to="/post/create">Create a post</Link><br/>
+            <Link to="/myposts">My Posts</Link><br/>
         </div>
     )
 }
