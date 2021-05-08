40f956359514edfe4126932d6ba95278b543e16e
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-08 15:24:07 +0530

    minor changes

--- client/src/pages/public/Home/heroSection/HeroSection.js
+++ client/src/pages/public/Home/heroSection/HeroSection.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function HeroSection() {
     return (
         <div className="HeroSection">
-            <h1 className="display-4">Home</h1>
-            <Link to={'/signup'} className="btn btn-success">Sign Up</Link>
-            <Link to={'/login'} className="btn btn-outline-info">Login</Link>
+            <h1 className="display-4">Welcome</h1>
+            {/* <Link to={'/signup'} className="btn btn-success">Sign Up</Link>
+            <Link to={'/login'} className="btn btn-outline-info">Login</Link> */}
         </div>
     )
 }
