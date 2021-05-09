b4c79c6609e7e62f5c6f344e2ef842251de1bc1b
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-09 20:06:45 +0530

    Get particular post of a particular user filtered by userId and postId. Add authToken middleware to check for authentication in some endpoints

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,5 +1,6 @@
 const Router = require("express").Router();
 const Post = require("../../model/post");
+const authToken = require("../api/authToken");
 
 // get posts data of a particular user filtered by id
 Router.get('/:id', (req, res) => {
@@ -17,6 +18,25 @@ Router.get('/', (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
+// get particular post of a particular user filtered by userId and postId
+const findPostByIdAndRespond = (res, postsData, postId) => {
+    postsData.posts.forEach((post) => {
+        if(post._id.toString() === postId) res.status(200).json(post);
+    });
+    
+    res.status(404).send('post not found!');
+}
+
+Router.get('/:userId/:postId', (req, res) => {
+    const userId = req.params.userId;
+    const postId = req.params.postId;
+
+    Post.findOne({ _id: userId })
+    .then(userPosts => findPostByIdAndRespond(res, userPosts, postId))
+    .catch(err => res.status(500).send(err))
+})
+
+
 // add a new post
 // the following is the json request format in the body
 // {
@@ -28,8 +48,7 @@ Router.get('/', (req, res) => {
 //     },
 //     "postedOn": null
 // }
-
-Router.post('/:id', (req, res) => {
+Router.post('/:id', authToken, (req, res) => {
     const userId = req.params.id;
     const newPostData = req.body.post;
 
@@ -49,11 +68,16 @@ Router.post('/:id', (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
-// edit a post
-Router.post('/edit/:userId/:postId', (req, res) => {
+
+// ************ PUT requests ************
+// edit data of a post
+Router.put('/edit/:userId/:postId', authToken, (req, res) => {
+    const userId = req.params.userId;
+    const postId = req.params.postId;
+    
     // const editPostData = req.body.post;
 
-    // const editPost = {  
+    // const editPost = { 
     //     posts: [{
     //         post: {
     //             image: {img: newPostData.post.image.img, placeholder: newPostData.post.image.placeholder},
@@ -67,8 +91,10 @@ Router.post('/edit/:userId/:postId', (req, res) => {
     // Post.updateOne({_id: newPostData.userId}, {$push: newPost})
     // .then(() => res.status(200).json({posted: true}))
     // .catch(err => res.status(500).send(err));
-
-    res.send(`userId: ${req.params.userId}\npostId: ${req.params.postId}`);
+    
+    res.send(`userId: ${userId}\npostId: ${postId}`);
 });
+// **************************************
+
 
 module.exports = Router;
