4430828a08e91dc4207a21f23b2a7726df2e9814
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-10 12:49:43 +0530

    make component which is rendered when post is not found

---
+++ client/src/pages/private/EditPost/PostNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostNotFound = () => {
+    return (
+        <>
+            <p className="text-danger text-uppercase">Post Not Found!</p>
+        </>
+    )
+}
+
+export default PostNotFound
