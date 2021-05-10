03729debfa4d08fc05f05e524ed3d7b4dc2a5b22
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-10 20:06:01 +0530

    add component when post created

---
+++ client/src/pages/private/CreatePost/PostCreated.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostCreated = () => {
+    return (
+        <div>
+            <h1 className="text-center display-4 text-success">Post Created</h1>
+        </div>
+    )
+}
+
+export default PostCreated
