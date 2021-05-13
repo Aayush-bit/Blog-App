0a5c988503f897571a6565784fc54fe688f4150d
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-13 22:18:07 +0530

    add page loader, pass required props to ShowProfilePosts component

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -2,54 +2,48 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
+import PageLoader from '../../components/PageLoader'
+import ShowProfilePosts from '../../components/ShowProfile/ShowProfilePosts'
+
 const User = () => {
     const { userId } = useParams();
     const url = `/api/users/profile/${userId}`;
+    
+    const [isLoading, setIsLoading] = useState(true);
     const [userData, setUserData] = useState({});
+    const [posts, setPosts] = useState([]);
     
     useEffect(() => {
         axios.get(url)
         .then(res => {
             setUserData(res.data[0]);
-            console.log(res.data[0]);
+            setPosts(res.data[0].postsData[0].posts);
+            setIsLoading(false);
         })
         .catch(resErr => console.error(resErr));
     }, []);
     
     return (
+        isLoading ? 
+        <PageLoader /> :
         <div className="container">
-            <h1 className="display-4">User</h1>
-            {/* <p>{userId}</p> */}
-            {/* <p>{JSON.stringify(userData._id)}</p> */}
-            <p>{JSON.stringify(userData.name)}</p>
-            <p>following: {JSON.stringify(userData.following)}</p>
-            <p>followers: {JSON.stringify(userData.followers)}</p>
-            <p>{JSON.stringify(userData.profileImg)}</p>
-            <p>bio: {JSON.stringify(userData.bio)}</p>
+            {/* show userDetails */}
+            <h1 className="display-4">{userData.name}</h1>
+            <p>following: {JSON.stringify(userData.following.length)}</p>
+            <p>followers: {JSON.stringify(userData.followers.length)}</p>
+            {/* <p>{JSON.stringify(userData.profileImg)}</p> */}
+            <p>Bio: {JSON.stringify(userData.bio)}</p>
             <p>{JSON.stringify(userData.email)}</p>
 
-            {/* todo - set page loader */}
-            <p>{JSON.stringify(userData.postsData[0].posts)}</p>
-            {/* {
-                userData.postsData[0].posts.map(post => (<p>{post}</p>))
-            } */}
-            
+            <hr />
+
+            {/* user's posts */}
+            <ShowProfilePosts 
+            author={userData.name} 
+            authorId= {userData._id}
+            posts={posts} />
         </div>
     )
 }
 
 export default User
-
-// db.users.aggregate([
-//     {
-//         $lookup: {
-//             from: "posts",
-//             localField: "_id",
-//             foreignField: "_id",
-//             as: "userDetail"
-//         }
-//     }, 
-//     {
-//         $project: {password: 0}
-//     }
-// ]);
