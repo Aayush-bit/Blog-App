42778cb9407004777ff40aa5836ee6a43e328a24
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-15 00:11:44 +0530

    make page to edit profile

---
+++ client/src/pages/private/EditProfilePage/EditProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import EditProfile from '../../../components/EditProfile/EditProfile'
+
+const EditProfilePage = () => {
+    return (
+        <div className="container">
+            <EditProfile />
+        </div>
+    )
+}
+
+export default EditProfilePage
