d7fb46c58195d7dda31c054b6403a63096a0282c
Aayush Kumar Mahajan <mahajanaayush23@gmail.com>
2021-05-20 09:40:31 +0530

    make and render sign up form component

---
+++ client/src/components/SignUp/SignUpForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const SignUpForm = ({setData, submitStatus, setSubmitStatus}) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignUpSubmit = (e) => {
+        e.preventDefault();
+        setData({ name, email, password });
+        setSubmitStatus(true);
+    }
+
+    return (
+        <div>
+            <Form method="POST" onSubmit={handleSignUpSubmit}>
+                <Form.Group controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control 
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter name"
+                    required />
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control 
+                    type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter email"
+                    required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                    type="password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required />
+                </Form.Group>
+
+                {
+                    submitStatus ?
+                    <Button variant="primary" type="submit" disabled>
+                        Please Wait...
+                    </Button> :
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                }
+
+            </Form>
+        </div>
+    )
+}
+
+export default SignUpForm
--- client/src/pages/public/SignUp.js
+++ client/src/pages/public/SignUp.js
@@ -1,76 +1,37 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import axios from 'axios'
-import { Form, Button } from 'react-bootstrap'
+import SignUpForm from "../../components/SignUp/SignUpForm"
 import { LoggedInContext } from '../../App'
 
 function SignUp() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [data, setData] = useState({})
-    const [loggedIn, setLoggedIn] = useContext(LoggedInContext)
+    const [data, setData] = useState({});
+    const [submitStatus, setSubmitStatus] = useState(false);
+    const [loggedIn, setLoggedIn] = useContext(LoggedInContext);
     const history = useHistory();
     
     useEffect(() => {        
-        if(name !== '' && email !== '' && password !== '') {
+        if(submitStatus) {
             const url = '/auth/signup'
             axios.post(url, data)
             .then(res => {
-                setLoggedIn(res.data.loggedIn)
+                setLoggedIn(res.data.loggedIn);
                 // redirect to dashboard page 
                 history.push('/dashboard');
             })
-            .catch(() => window.location.reload())
+            .catch(() => window.location.reload());
         }
-    }, [data])
+    }, [submitStatus])
 
-    const handleSignUpSubmit = (e) => {
-        e.preventDefault();
-        setData({ name, email, password });
-    }
-    
     return (
         <div className="container">
             <h1 className="display-4 mb-3">Sign Up</h1>
-            <Form method="POST" onSubmit={handleSignUpSubmit}>
-                <Form.Group controlId="formBasicName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control 
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter name"
-                    required />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                    type="email" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter email"
-                    required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                    type="password" 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                    required />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+            <SignUpForm 
+            submitStatus={submitStatus}
+            setSubmitStatus={setSubmitStatus}
+            setData={setData} />
             <hr/>
+
             <p className="text-muted">
                 Already have an account? <Link to='/login'>Login</Link>
             </p>
