dcb2f97f02a6d4af3ded0137382526dc1bb8f93f
Aayush Mahajan <mahajanaayush23@gmail.com>
2021-10-29 22:58:36 +0530

    minor change

--- client/src/pages/private/MyPosts/NoPostsPresent.js
+++ client/src/pages/private/MyPosts/NoPostsPresent.js
@@ -5,8 +5,6 @@ const NoPostsPresent = () => {
     return (
         <div>
             <p>No Posts Present!!</p>
-            <Link to="/post/create">Create a post</Link>
-
         </div>
     )
 }
