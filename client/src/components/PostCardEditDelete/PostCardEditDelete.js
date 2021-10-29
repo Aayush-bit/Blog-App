2d4ba87a943b80d0076f188e1ac6d5a224b6e94d
Aayush Mahajan <mahajanaayush23@gmail.com>
2021-10-29 11:24:42 +0530

    enabled edit and delete functionality

--- client/src/components/PostCardEditDelete/PostCardEditDelete.js
+++ client/src/components/PostCardEditDelete/PostCardEditDelete.js
@@ -3,9 +3,12 @@ import { useHistory } from 'react-router-dom'
 import {Button} from "react-bootstrap"
 import "./PostCardEditDelete.css"
 import {Row, Col, Container} from "react-bootstrap";
+import axios from 'axios';
 
 const PostCardEditDelete = ({post, authorId, author}) => {
     const history = useHistory()
+
+    const [isDeleted, setIsDeleted] = useState(false);
 
     // shows breif of the entered data string
     const showBrief = (dataString, length) => {
@@ -18,28 +21,42 @@ const PostCardEditDelete = ({post, authorId, author}) => {
         history.push(`/post/${authorId}/${post._id}`);
     }
     
+    const deletePost = () => {
+        // console.log("deleting...");
+
+        const postId = post._id;
+        
+        axios.delete(`/api/posts/${authorId}/${postId}`)
+        .then(() => setIsDeleted(true))
+        .catch(resErr => console.error(resErr));
+    }
 
     return (
+        isDeleted ? <></> :
         <div className="container postCardEditDelete">
             <Container>
                 <Row>
-                    <Col sm={8} className="postCardEditDelete__title">
-                        <h3>{showBrief(post.post.title, 50)}</h3>
+                    <Col sm={8} className="postCardEditDelete__title text-capitalize">
+                        <h3>{showBrief(post.post.title, 40)}</h3>
                     </Col>
 
                     <Col sm={4} className="postCardEditDelete__actions">
                         <Button variant="light" size="sm" onClick={showFullPost}>
                             View
-                        </Button>
+                        </Button>                        
 
-                        <div className="postCardEditDelete__action postCardEditDelete__actions__edit">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <div 
+                        className="postCardEditDelete__action postCardEditDelete__actions__edit"
+                        onClick={() => history.push(`/post/edit/${post._id}`)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                             </svg>
                         </div>
                         
-                        <div className="postCardEditDelete__action postCardEditDelete__actions__delete">
+                        <div 
+                        onClick={deletePost} 
+                        className="postCardEditDelete__action postCardEditDelete__actions__delete">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                 <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -48,7 +65,7 @@ const PostCardEditDelete = ({post, authorId, author}) => {
                     </Col>
                 </Row>
                 <Row>
-                    <Col className="">
+                    <Col>
                         <div className="postCardEditDelete__author text-muted text-capitalize">
                             <small>by- {author}</small>
                         </div>
@@ -68,47 +85,6 @@ const PostCardEditDelete = ({post, authorId, author}) => {
                     </Col>
                 </Row>
             </Container>
-
-            
-            {/* <div className="postCardEditDelete__box">
-                <div className="postCardEditDelete__content">
-                    <div className="postCardEditDelete__title">
-                        {showBrief(post.post.title, 50)}
-                    </div>
-                    <div className="postCardEditDelete__author text-muted text-capitalize">
-                        <small>by- {author}</small>
-                    </div>
-                    <div className="postCardEditDelete__content">
-                        {showBrief(post.post.content, 200)}
-                    </div>
-                </div>
-                <div className="postCardEditDelete__actions">
-                    <Button variant="primary" size="sm">
-                        View
-                    </Button>
-                    <div className="postCardEditDelete__action postCardEditDelete__actions__delete">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                    </div>
-                    <div className="postCardEditDelete__action postCardEditDelete__actions__edit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                        </svg>
-                    </div>
-                </div>
-                <div className="postCardEditDelete__info">
-                    <div className="postCardEditDelete__date-time text-muted">
-                        {
-                            post.editedOn ? 
-                            post.editedOn.split("T")[0] + " (edited)": 
-                            post.postedOn.split("T")[0]
-                        }
-                    </div>
-                </div>
-            </div> */}
         </div>
     )
 }
